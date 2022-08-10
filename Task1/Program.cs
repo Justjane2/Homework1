697b2993277a54a4fc8ed5c1bbffdacfdde0ac66
Evgenia Korystova <korystova@gmail.com>
2022-08-10 22:23:21 +0300

    task1 done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 5;
+int b = 7;
+int max = a;
+if (a>max) max = a;
+if (b>max) max = b;
+Console.WriteLine(max);
+

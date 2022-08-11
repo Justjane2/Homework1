67fcc3bfaa0acf685904f0739a820422cef06e1f
Evgenia Korystova <korystova@gmail.com>
2022-08-11 06:54:34 +0300

    Completed task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите целое число: ");
+
+int a = int.Parse(Console.ReadLine());
+
+if(a % 2 == 0)
+
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}

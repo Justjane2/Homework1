2e9dfcda0924a99dbabd03949e6771b95ed19c51
Evgenia Korystova <korystova@gmail.com>
2022-08-11 10:15:10 +0300

    Tried to do ex4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,3 +1,8 @@
 ﻿Console.Write("Введите целое число: ");
-
-int a = int.Parse(Console.ReadLine());
+int value = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while(count <= value)
+{
+    Console.WriteLine(count);
+    count + 2;
+}
